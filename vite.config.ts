da8e26f2aabfdbaaa2675131943c388818c48af5
hassan-088 <ma7397882@gmail.com>
2026-04-12 23:21:42 +0200

    fix vercel routing FINAL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // ✅ MUST be "/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
